ce9375ab61ae8b97641d394be4bb0b708675ce3f
naricane <git@naricane.com>
2026-08-23 21:28:58 +0200

    Add backface culling

--- src/rasterize.cpp
+++ src/rasterize.cpp
@@ -93,6 +93,12 @@ edge_function(Vec2i a, Vec2i b, Vec2i c)
 void
 fill_triangle(Framebuffer& fb, Vec2i a, Vec2i b, Vec2i c)
 {
+	int ABC = edge_function(a, b, c);
+
+	if (ABC < 0) {
+		return;
+	}
+
 	int min_x = std::min(std::min(a.x, b.x), c.x);
 	int min_y = std::min(std::min(a.y, b.y), c.y);
 	int max_x = std::max(std::max(a.x, b.x), c.x);
@@ -100,14 +106,14 @@ fill_triangle(Framebuffer& fb, Vec2i a, Vec2i b, Vec2i c)
 
 	uint32_t color = 0x0;
 
-	for (int y = min_y; y < max_y; ++y) {
-		for (int x = min_x; x < max_x; ++x) {
+	for (int y = std::max(min_y, 0); y < std::min(max_y, HEIGHT); ++y) {
+		for (int x = std::max(min_x, 0); x < std::min(max_x, WIDTH); ++x) {
 			Vec2i p{ x, y };
 			int ABP = edge_function(a, b, p);
 			int BCP = edge_function(b, c, p);
 			int CAP = edge_function(c, a, p);
 
-			if (ABP >= 0 && BCP >= 0 && CAP >= 0) {
+			if (ABP <= 0 && BCP <= 0 && CAP <= 0) {
 				put_pixel(fb, Vec2i{ p.x, p.y }, color);
 			}
 		}
