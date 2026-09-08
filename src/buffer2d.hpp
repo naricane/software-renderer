59e48d561da407cb26bb7f1775c370c9fb13f2d2
naricane <git@naricane.com>
2026-09-08 19:32:51 +0200

    Add deleted copy constructor to the Buffer2d

--- src/buffer2d.hpp
+++ src/buffer2d.hpp
@@ -15,6 +15,10 @@ public:
 		: data(WIDTH * HEIGHT, T{})
 	{
 	}
+	Buffer2D(const Buffer2D&) = delete;
+	Buffer2D& operator=(const Buffer2D&) = delete;
+	Buffer2D(Buffer2D&&) = default;
+	Buffer2D& operator=(Buffer2D&&) = default;
 
 	void clear(T value = T{}) { std::fill(data.begin(), data.end(), value); }
 	std::span<T> get_span() { return std::span{ data }; }
