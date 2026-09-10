df5730d2a0d072ab9218241d7dc3681e8b43d4ca
naricane <git@naricane.com>
2026-09-10 17:22:01 +0200

    Change files hierarchy

--- src/core/constants.hpp
+++ src/core/constants.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <cstdint>
 
-constexpr int WIDTH = 800;
-constexpr int HEIGHT = 600;
+constexpr int WIDTH = 320 * 2;
+constexpr int HEIGHT = 180 * 2;
 
 constexpr uint32_t LINE_COLOR = 0xFFFFFFFF;
